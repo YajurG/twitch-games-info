d3e55da02ad96818536eb1729b66349ae7eaace7
YajurG <yajurgrover24@gmail.com>
2021-04-18 00:38:55 +0800

    added request from client

--- back-end/server.js
+++ back-end/server.js
@@ -18,6 +18,6 @@ app.get("/games", data.getGames)
 
 app.get("/streams/:gameId/:numOfStreams", data.getStreams)
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
 })
