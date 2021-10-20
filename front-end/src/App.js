c0afc611b5b9534a7dcc35f6158df8abe588cef3
YajurG <yajurgrover24@gmail.com>
2021-10-19 20:19:26 -0500

    storing twitch token in localstorage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,42 @@
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+const App = () => {
+
+  useEffect(() => {
+    async function getTwitchToken() {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/token`);
+        if (res.status != 200) {
+          console.log("token access denied")
+          return;
+        }
+        try {
+          localStorage.setItem("twitchToken", JSON.stringify(response.token));
+          let expiry_date = new Date();
+          expiry_date.setSeconds(expiry_date.getSeconds + parseInt(response.expires_in));
+          localStorage.setItem("twitchTokenExpiry", expiry_date.toString());
+        } catch (err) {
+          console.log(err.message);
+        }
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    let expiryString = localStorage.getItem("twitchTokenExpiry");
+    const expiry = new Date(expiryString);
+    const now = new Date();
+    const difference = expiry - now;
+    if (difference > 1800000) { // 30 minutes
+        getTwitchToken();
+    }
+  }, [])
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ front-end/src/Navbar.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './index.css';
+import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.css";
+import axios from 'axios';
+
+const Navigation = () => {
+    return (
+        <Navbar collapseOnSelect bg='dark' variant='dark'>
+            <Container>
+            <Navbar.Brand href="/home">Twitch Information Dashboard</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav>
+                    <Nav.Link href="/home">Home</Nav.Link>
+                </Nav>
+                <Form inline className="ml-auto">
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+const searchGames = (text) => {
+    axios.get("http://localhost:8080/games")
+}
+
+export default Navigation;
