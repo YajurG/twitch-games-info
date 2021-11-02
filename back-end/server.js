9bc3ebade3a098c31e5caa843c4bac4f494e6531
YajurG <yajurgrover24@gmail.com>
2021-11-01 22:51:30 -0500

    can submit register request from front end

--- back-end/server.js
+++ back-end/server.js
@@ -25,6 +25,8 @@ db.mongoose.connect(dbConfig.CONNECTION_STRING, {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
--- front-end/src/components/register.js
+++ front-end/src/components/register.js
@@ -8,25 +8,41 @@ const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [valid, setValid] = useState(false);
+    const [error, setError] = useState(false);
 
     function validateForm() {
         return username.length > 0 && password.length > 0;
     }
     
-    async function onSubmit() {
-        console.log("this is the submit method")
+    async function onSubmit(e) {
+        e.preventDefault();
+        if (username == null || password == null) {
+            console.log("")
+        }
+        console.log(username);
+        const res = await axios.post("http://localhost:8080/api/register", {username: username, password: password});
+        console.log(res);
+    }
+
+    const usernameOnChange = (e) => {
+        setUsername(e.target.value);
+        console.log(username);
+    }
+
+    const passwordOnChange = (e) => {
+        setPassword(e.target.value);
     }
     
     return (
         <div style={{textAlign: "left", marginLeft: "10px", marginRight: "10px"}}>
             <h3 style={{textAlign: "center"}}>Register</h3>
             <div>
-                <form className="form-inline">
+                <form className="form-inline" onSubmit={onSubmit}>
                     <Form.Group className="mb-3">
-                        <Form.Control type="username" placeholder="Enter username here"></Form.Control>
+                        <Form.Control type="username" placeholder="Enter username here" onChange={usernameOnChange}></Form.Control>
                     </Form.Group>
                     <Form.Group className="mb-3">
-                        <Form.Control type="password" placeholder="Enter password here"></Form.Control>
+                        <Form.Control type="password" placeholder="Enter password here" onChange={passwordOnChange}></Form.Control>
                         <Form.Text className="text-muted">
                             <b>Password should be at least 8 characters</b>
                         </Form.Text>
