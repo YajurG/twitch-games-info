4757fdc83b13230ff6ef930bdb3439bf0e25c53f
YajurG <yajurgrover24@gmail.com>
2021-11-03 09:26:41 -0500

    removed config files

--- back-end/config/api.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    client_id: "vol0a1sflwouawfe4n6oypes67mass",
-    client_secret: "k5okbzkncl823w9vuhdczvqryu8256"
-}
--- back-end/config/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    CONNECTION_STRING: "mongodb+srv://rootUser:cs506@cluster0.j8faq.mongodb.net/twitch_data1w"
-  };
-  
